955ad61c5b8ad1a2e2015dc5a0cf6b7013f9d979
strange-developer <cbodasing@gmail.com>
2019-01-24 16:47:25 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+function createCache(apiCall, options) {
+  let cache = {};
+
+  function calculateExpiry(timeToLive) {
+    if (timeToLive === 'infinity') {
+      return 'infinity';
+    }
+    return new Date().getTime() + timeToLive;
+  }
+
+  const internalOptions = { timeToLive: calculateExpiry(options.timeToLive) };
+
+  async function makeCall(parameters) {
+    const result = { isSuccessful: undefined, result: undefined };
+    try {
+      result.result = await apiCall(...parameters);
+      result.isSuccessful = true;
+    } catch (error) {
+      result.result = error;
+      result.isSuccessful = false;
+    }
+    return result;
+  }
+
+  function shouldMakeApiCall() {
+    if (
+      internalOptions.timeToLive !== 'inifinity' &&
+      (Object.keys(cache).length === 0 || new Date().getTime() > internalOptions.timeToLive)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  async function read(...parameters) {
+    let response = cache;
+    if (shouldMakeApiCall(cache)) {
+      response = await makeCall(parameters);
+      cache = response;
+      internalOptions.timeToLive = calculateExpiry(options.timeToLive);
+    }
+    return response;
+  }
+
+  return { read };
+}
+
+export default createCache;
