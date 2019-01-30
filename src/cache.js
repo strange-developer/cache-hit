00465a99098dcb42fe1bb25cf1ab74eb559f42b5
Unknown <cbodasing@gmail.com>
2019-01-30 20:59:03 +0200

    feature: Add babel polyfill. Spread parameters.

--- src/cache.js
+++ src/cache.js
@@ -1,14 +1,14 @@
 import { shouldMakeApiCall, calculateExpiry } from './utils';
 
-function createCache(apiCall, options = {}) {
+const createCache = (apiCall, options = {}) => {
   const cache = {};
 
   const internalOptions = { timeToLive: calculateExpiry(options.timeToLive) };
 
   async function read(key, ...parameters) {
-    let response = cache;
+    let response = cache[key];
     if (shouldMakeApiCall(cache, key, internalOptions.timeToLive)) {
-      response = await apiCall(parameters);
+      response = await apiCall(...parameters);
       cache[key] = response;
       internalOptions.timeToLive = calculateExpiry(options.timeToLive);
     }
@@ -16,6 +16,6 @@ function createCache(apiCall, options = {}) {
   }
 
   return { read };
-}
+};
 
 export default createCache;
